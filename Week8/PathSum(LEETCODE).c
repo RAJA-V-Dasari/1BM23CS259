e4e0fba36937827c5718029f6acaf59b4a281abb
Raja Vishwanath Dasari <rajavishwanath.cs23@bmsce.ac.in>
2024-12-17 00:40:41 +0530

    Update PathSum(LEETCODE).c

--- Week8/PathSum(LEETCODE).c
+++ Week8/PathSum(LEETCODE).c
@@ -1,18 +1,26 @@
-bool preorderTraversal(struct TreeNode* root, int currentSum, int targetSum) {
-    
+#include <stdbool.h>
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool checkPathSum(struct TreeNode* root, int currentSum, int targetSum) {
+
     if (root == NULL) {
         return false;
     }
-
     currentSum += root->val;
-
     if (root->left == NULL && root->right == NULL) {
         return currentSum == targetSum;
     }
 
-    return preorderTraversal(root->left, currentSum, targetSum) ||
-           preorderTraversal(root->right, currentSum, targetSum);
+    return checkPathSum(root->left, currentSum, targetSum) ||
+           checkPathSum(root->right, currentSum, targetSum);
 }
+
 bool hasPathSum(struct TreeNode* root, int targetSum) {
-    return preorderTraversal(root, 0, targetSum);
+    return checkPathSum(root, 0, targetSum);
 }
